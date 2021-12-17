96ef8843e25a50886a431f19d0745b2a8d578b49
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-12-17 10:29:12 +0700

    create common components Head

---
+++ src/components/common/Head/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import NextHead from 'next/head';
+
+const Head: FC = () => (
+  <NextHead>
+    <title> Thanh Minh portfolio</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </NextHead>
+);
+
+export default Head;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+// components
+import Head from 'components/common/Head';
 // style
 import 'styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <React.Fragment>
+    <Head />
+    <Component {...pageProps} />
+  </React.Fragment>
 );
 
 export default MyApp;
