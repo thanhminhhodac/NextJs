e17e9eceefbaab757cbbd81714fb6d1c5f49e81c
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-12-20 16:38:40 +0700

    using Header Components

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,9 +1,19 @@
 // libs
 import React from 'react';
+// components
+import HeaderLogo from 'components/ui/HeaderLogo';
+import HeaderNav from 'components/ui/HeaderNav';
+import HeaderFunction from 'components/lib/HeaderFunction';
+// style
+import styles from './Header.module.scss';
 
 const Header = () => (
-  <div className="header-wrapper">
-    <div className="header-wrapper-inner">This is page header </div>
+  <div className={styles.wrapper}>
+    <div className={styles.wrapperInner}>
+      <HeaderLogo />
+      <HeaderNav />
+      <HeaderFunction />
+    </div>
   </div>
 );
 
